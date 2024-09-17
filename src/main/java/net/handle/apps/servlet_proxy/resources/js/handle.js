001d044aa4fd1a3a3a80ca7a803eec8e7bf9d0c6
asajedi <ali.sajedi@gwdg.de>
2024-09-17 16:49:59 +0200

    Add ISNI provider to UI

--- src/main/java/net/handle/apps/servlet_proxy/resources/js/handle.js
+++ src/main/java/net/handle/apps/servlet_proxy/resources/js/handle.js
@@ -58,6 +58,11 @@ function processHandleOnPaste() {
             document.getElementById("metadata").disabled=false;
             document.getElementById("resource").disabled=true;
         }
+        // ISNI RegEX
+        if (identifier.match(/^(\d{15}[1-9X])$/)) {
+            document.getElementById("metadata").disabled=true;
+            document.getElementById("resource").disabled=true;
+        }
         if ((identifier.match(/^10\.\d+\/.+$/) || identifier.match(/(d|D)(o|O)(i|I):10\.\d+\/.+$/)) && !identifier.includes("zenodo")) {
             document.getElementById("content-negotiation-title").style.display = "block";
             document.getElementById("content-negotiation-bibtex").style.display = "block";
@@ -178,6 +183,12 @@ for (var i=0; i < templateHandleIds.length; i++) {
                 document.getElementById("content-negotiation-turtle").style.display = "none";
                 document.getElementById("content-negotiation-rdf").style.display = "none";
             }
+            // ISNI RegEx
+            if (identifier.match(/^(\d{15}[1-9X])$/)) {
+                document.getElementById("metadata").disabled=true;
+                document.getElementById("resource").disabled=true;
+            }
+
             document.getElementById("identifier").value = identifier;
 
             if (!identifier.includes("21.")) {
